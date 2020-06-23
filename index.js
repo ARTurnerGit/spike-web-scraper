b81e6458121f2d4e84302bdb0e72d3ebe6d00be0
Alun Turner <alun.turner@googlemail.com>
2020-06-23 14:36:40 +0100

    spike complete

--- index.js
+++ index.js
@@ -1,24 +1,21 @@
 const myRequest = new Request(
-  "https://dominator-proxy-server.herokuapp.com/997721"
+  "https://dominator-proxy-server.herokuapp.com/997721/gamelog"
 );
 
 // going to change the proxy-server so that data comes back as JSON:
 // let JSON = {
-//   gameLog: ["", "", ""],
-//   playerColours: ["", "", ""],
-//   mapUrl: "",
-//   territory: { terr1: {}, terr2: {} },
+//   gamelog: ["", "", ""],
+//   players: {playername:{}...},
+//   map: {url:'', width:'', height: ''},
+//   territory: { terr1: {}, terr2: {}, ... },
 // };
 
 fetch(myRequest)
   .then((res) => {
     console.log("got a response");
-    return res.text();
+    return res.json();
   })
   .then((data) => {
-    let parser = new DOMParser();
-    let doc = parser.parseFromString(data, "text/html");
-    let loadlog = doc.querySelector("#load-log");
-    console.dir(loadlog);
+    console.dir(data);
   })
   .catch((err) => console.log(err));
